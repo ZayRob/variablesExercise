68a6609acd648fa1c9cbeef6519ca8d4c83a81a0
Isaiah Robinson <isaiahrobinson@KCCCs-MacBook-Pro-8.local>
2021-02-23 13:35:51 -0500

    Completed variables exercise

---
+++ variablesExercise.js
@@ -0,0 +1,20 @@
+// Const is used for a variable with a value that will not change
+
+let num = 20;
+
+num = 20 - 5;
+
+num += 7;
+
+num--;
+
+num = "This is not a number";
+
+let blank;
+
+console.log(blank);
+
+blank = null;
+
+
+
